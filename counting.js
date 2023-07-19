13e16b9e3a1c2a700035fe2c195cf294e067f53a
Kevinemug <kevineeuwamahoro@gmail.com>
2023-07-19 11:13:57 +0200

    counting

---
+++ counting.js
@@ -0,0 +1,11 @@
+const countingZeros = n => {
+  let count = 0;
+  let divider = 10;
+
+  while (n >= divider) {
+    count += Math.floor(n / divider);
+    divider *= 10;
+  }
+
+  return count;
+}
